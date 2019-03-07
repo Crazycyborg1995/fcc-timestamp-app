03de4e05b9bc1bca28af6bd7b6d045488c19cc11
Crazycyborg1995 <mhdafsalvp@gmail.com>
2019-03-07 19:31:33 +0530

    renamed app.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/', function(req, res) {
 
 app.get('/api/timestamp/:date_string', (req, res) => {
   let date = new Date(req.params.date_string);
+  
   res.status(200).send(timeStamp(date));
 });
 
